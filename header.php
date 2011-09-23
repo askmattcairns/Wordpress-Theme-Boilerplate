be3cdea535e32d1479b3d69bb3dcf1a49f4d19a2
Matt Cairns <opusmatt@gmail.com>
2011-09-22 22:32:57 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
+	
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	
+	<?php wp_head(); ?>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,5 @@
+<php get_header(); >
+	
+	<!-- Site content goes here -->
+	
+<?php get_footer(); ?>
